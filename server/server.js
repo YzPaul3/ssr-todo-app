a10a1342efe16590dfd9b683f9ec3f24f93e24f5
heyuze_dxm <heyuze@duxiaoman.com>
2018-09-10 15:13:40 +0800

    config babel-register babel-polyfill to use async/await on Node v6+

--- server/server.js
+++ server/server.js
@@ -1,41 +1,5 @@
-const Koa = require('koa')
-const send = require('koa-send')
-const path = require('path')
-
-const pageRouter = require('./routers/dev-ssr')
-const app = new Koa()
-
-const isDev = process.env.NODE_ENV === 'development'
-
-// 记录所有的请求及错误，并且返回一个错误信息
-app.use(async (ctx, next) => {
-  try {
-    console.log(`request with path ${ctx.path}`)
-    await next()
-  } catch (err) {
-    console.log(err)
-    ctx.status = 500
-    if (isDev) {
-      ctx.body = err.message
-    } else {
-      ctx.body = 'please try again later'
-    }
-  }
+require('babel-polyfill')
+require('babel-register')({
+  ignore: /node_modules\/(?!koa-send)/
 })
-
-app.use(async (ctx, next) => {
-  if (ctx.path === '/favicon.ico') {
-    await send(ctx, '/favicon.ico', { root: path.join(__dirname, '../') })
-  } else {
-    await next()
-  }
-})
-
-app.use(pageRouter.routes()).use(pageRouter.allowedMethods())
-
-const HOST = process.env.HOST || '0.0.0.0'
-const PORT = process.env.PORT || 3333
-
-app.listen(PORT, HOST, () => {
-  console.log(`server is listening on ${HOST}:${PORT}`)
-})
+require('./app.js')
